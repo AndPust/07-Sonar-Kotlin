07e88bae04582d42e1f08b8353c29941d57fef29
Andrzej Pustelnik <andrzej.pustelnik@student.uj.edu.pl>
2024-09-12 13:54:24 +0200

    Adding ktlint adn github workflow

--- kotlinBackend/build.gradle.kts
+++ kotlinBackend/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     application
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.serialization") version "1.5.31"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("org.sonarqube") version "3.5.0.2730"
 }
 
 group = "com.example"
@@ -30,4 +32,26 @@ dependencies {
     implementation("io.ktor:ktor-server-cors:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+ktlint {
+    version.set("0.48.2")
+    verbose.set(true)
+    android.set(false)
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+    enableExperimentalRules.set(true)
+}
+
+tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask>().configureEach {
+    reporterOutputDir.set(project.layout.buildDirectory.dir("reports/ktlint"))
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "AndPust_07-Sonar-Kotlin")
+        property("sonar.organization", "andpust")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.kotlin.ktlint.reportPaths", "build/reports/ktlint/**/*.txt")
+    }
 }
